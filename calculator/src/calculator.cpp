1790b4aa2e98c202509e82c155e26e49998deb63
tarinya <muriele@trentini.ch>
2016-10-21 09:10:29 +0200

    adds calculator

---
+++ calculator/src/calculator.cpp
@@ -0,0 +1,34 @@
+#include <stdexcept>
+#include <regex>
+#include <iostream>
+
+int calc(int i, int j, char c) {
+	switch (c) {
+	case '+':
+		return i + j;
+	case '-':
+		return i - j;
+	case '*':
+		return i * j;
+	case '/':
+		if (j == 0) {
+			return -1;
+		}
+		return i / j;
+	case '%':
+		if (j == 0) {
+			return -1;
+		}
+		return i % j;
+	default:
+		return -1;
+	}
+}
+
+int calc(std::istream& in) {
+	int n1{}, n2{};
+	char op{};
+	in >> n1 >> op >> n2;
+	return calc(n1,n2,op);
+}
+
---
+++ calculator/src/calculator.h
@@ -0,0 +1,8 @@
+#ifndef CALCULATOR_H_
+#define CALCULATOR_H_
+
+#include <iosfwd>
+int calc(int, int, char);
+int calc(std::istream& in);
+
+#endif
